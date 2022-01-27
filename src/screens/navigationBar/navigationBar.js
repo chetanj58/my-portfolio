377ae203cdb56b27cdf59b4c9fd831ba4f75d633
chetanj58 <chetanj58@gmail.com>
2022-01-27 13:18:46 +0530

    added navigation bar

---
+++ src/screens/navigationBar/navigationBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './navigation.css';
+import {navigationLinks} from '../../helpers/navigationLinks';
+
+function createLinks() {
+  return navigationLinks.map((e, idx) =>(
+    <Nav.Link key={idx} href={e.ref}>{e.name}</Nav.Link>
+  ));
+}
+
+function navigationBar() {
+  return (
+    <div id='home'>
+      <Navbar className="navigation__container" expand="md">
+      <Navbar.Brand style={{marginLeft:"1rem"}} href="#home">Chetan Jadhav</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse style={{justifyContent: 'flex-end', marginRight:'1rem', borderColor:'none'}}>
+      <Nav className='links' style={{margin:'0 1rem'}}>
+      {createLinks()}      
+      </Nav>
+      </Navbar.Collapse>
+      
+      </Navbar>
+    </div>
+  )
+}
+
+export default navigationBar
